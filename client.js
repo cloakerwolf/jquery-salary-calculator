a226231dd80ee08b5df6fe9a20e1cedd675608ed
Alex Smith <alexesmith@icloud.com>
2019-07-26 18:19:27 -0500

    first function printing with logs

--- client.js
+++ client.js
@@ -12,10 +12,23 @@ function newEmployee() {
     let ID = $('#idInput').val();
     let title = $('#titleInput').val();
     let annualSalary = $('#annualSalaryInput').val();
-
-    
+ 
+    // make this one show on dom
+    $('.employee').append(`<tr>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${ID}</td>
+    <td>${title}</td>
+    <td>${annualSalary}</td>
+    <td id = "delete">
+    </tr>`);
+    console.log(firstName);
+    console.log(lastName);
+    console.log(ID);
+    console.log(title);
+    console.log(annualSalary);
 }
 function readyNow() {
     console.log('Dom');
-
+    $('#dataCollection').on('click', newEmployee);
 }
