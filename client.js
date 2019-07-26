7631f16ee39ec9ced56b7bf69fbfec2da1021057
Alex Smith <alexesmith@icloud.com>
2019-07-26 17:25:28 -0500

    started

---
+++ client.js
@@ -0,0 +1,21 @@
+console.log('client.js');
+
+$(document).ready(readyNow);
+
+
+
+
+function newEmployee() {
+    console.log('newEmployee');
+    let firstName = $('#firstNameInput').val();
+    let lastName = $('#lastNameInput').val();
+    let ID = $('#idInput').val();
+    let title = $('#titleInput').val();
+    let annualSalary = $('#annualSalaryInput').val();
+
+    
+}
+function readyNow() {
+    console.log('Dom');
+
+}
