6a8f90a0935e4271133f8e727a22bd01e3635f2d
Alex Smith <alexesmith@icloud.com>
2019-07-29 07:45:38 -0500

    rounding the number

--- client.js
+++ client.js
@@ -13,7 +13,7 @@ function newEmployee() {
     let title = $('#titleInput').val();           //takes the title input
     let annualSalary = $('#annualSalaryInput').val();  // takes the annualSalary input
     let monthlyCost = annualSalary/12;             //creates monthlyCost
-    totalMonthlyCost += monthlyCost;     // adds monthly cost to the global totalMonthlyCost
+    totalMonthlyCost += Math.round(100 * monthlyCost) / 100;     // adds monthly cost to the global totalMonthlyCost
     // make this one show on dom
     $('.employee').append(`<tr>
     <td>${firstName}</td>
@@ -38,21 +38,16 @@ function newEmployee() {
 }
 
 function deleteEmployee() {
-    console.log('delete');
+    //console.log('delete');
+    // find the monthly cost of the line about to be deleted
     
-     $(this).parent().parent().remove();
+     $(this).parent().parent().remove(); //targets and removes the parent of the parent of the delete that i click if i just do $(this).remove and click the button it would only delete the button
 }
 
 
 
-
-
-
-
-
-
 function readyNow() {
-    console.log('Dom');
-    $('#dataCollection').on('click', newEmployee);
-    $('.employee').on('click','#delete', deleteEmployee);
+   // console.log('Dom');
+    $('#dataCollection').on('click', newEmployee); //dataCollection is the submit button when clicked run newEmployee function
+    $('.employee').on('click','#delete', deleteEmployee); //when i click on a delete button for a specific employee it runs the deleteEmployee function
 }
